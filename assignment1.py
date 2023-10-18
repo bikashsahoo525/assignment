1eed3d50229a5086a291aff3f7e962b50a8df996
BIKASH KUMAR SAHOO <138684026+bikashsahoo525@users.noreply.github.com>
2023-10-18 21:20:01 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+
+# In[ ]:
+
+
+* - expressions
+
+-87.8-value
+- -expression
+(-, value)
++ - expression
+6 - value
+
+
+# 2. What is the difference between string and variable?
+
+# In[ ]:
+
+
+A variable can be of several types like integer, string, float, etc. 
+based on the value you assign to it.
+anything you assign to a variable using quotes (single or double) will be considered as a string.
+
+Eg, numA = 23(variable) 
+    A = (“dhoni”) (string)
+
+
+# 3. Describe three different data types.
+
+# In[ ]:
+
+
+Ans. int - A positive or negative whole number without a decimal point( a = 50)
+floating point- A number witha decimal point.(b=22.5)
+string - any text with in two single or double quotes.(c=“bikash”)
+
+
+# 4. What is an expression made up of? What do all expressions do?
+
+# In[ ]:
+
+
+ans-Expression is made up of values, containers, and mathematical operators.
+Eg, a = 3+4 and expressions are used to evaluate the value.
+
+
+# 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+
+# In[ ]:
+
+
+Ans. The assignemnt statemets assign a value to the variable.
+in spam = 10 , value 10 in assigned to spam, and now spam will have value 10.
+But in expression a = C+B, the value of a depends on the value of C and B.
+
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+
+# In[ ]:
+
+
+Sol: it gives 23 as execution code 
+
+
+# 7. What should the values of the following two terms be?
+# spam' + 'spamspam'('spamspamspam')
+# 'spam' * 3('spamspamspam') 
+
+# In[ ]:
+
+
+sol: it will be the same 
+
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# In[ ]:
+
+
+ans-Variable name can not starts with numbers.
+Eg.a100 is valid
+
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+
+# In[ ]:
+
+
+ans: str(), int(), float()
+
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten' + 99 + 'burritos' 
+
+# In[ ]:
+
+
+Ans. 99 is an integer it cannot be concatenated with strings, if we have to 
+concatenate This can be fixed by converting 99 into string by str(99)
+
